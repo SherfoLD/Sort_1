52ca4dd7ff8402949e70f210e024df39a48b9551
SherfoLD <72308106+SherfoLD@users.noreply.github.com>
2022-10-29 00:04:37 +0300

    Base functions added

--- Sort_1/main.cpp
+++ Sort_1/main.cpp
@@ -1,14 +1,85 @@
-//
-//  main.cpp
-//  Sort_1
-//
-//  Created by Dmitry Savelev on 28.10.2022.
-//
-
 #include <iostream>
+#include <ctime>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct Node
+{
+    int value;
+    Node *next;
+};
+
+struct List
+{
+    Node *head;
+    Node *sorted;
+
+    void push(int val)
+    {
+        Node *startNode = new Node{val, NULL};
+        startNode->next = head; //показать где голова
+        head = startNode;
+    }
+
+    void print()
+    {
+        Node *node = head;
+        while (node)
+        {
+            std::cout << node->value << " ";
+            node = node->next;
+        };
+        std::cout << "/n";
+    }
+
+    void sort()
+    {
+        sorted = NULL;
+        Node *current = head;
+        while (current != NULL)
+        {
+            Node *next = current->next;
+            insert(current);
+            current = next;
+        }
+        head = sorted;
+    }
+
+    void insert(Node *nodeInsert)
+    {
+        if (sorted == NULL || nodeInsert->value <= sorted->value) //переопределяем голову в списке sorted
+        {
+            nodeInsert->next = sorted;
+            sorted = nodeInsert;
+        }
+        else
+        {
+            Node *current = sorted;
+            while (current->next != NULL && current->next->value < nodeInsert->value) //ищем куда положить в новом списке
+            {
+                current = current->next;
+            }
+            nodeInsert->next = current->next;
+            current->next = nodeInsert;
+        }
+    }
+};
+
+int main()
+{
+    List list;
+
+    list.head = NULL;
+    
+    for (int i = 0; i < 10; i++)
+    {
+        list.push(rand()%100);
+    };
+    
+    unsigned long sortTimeStart =  clock();
+    list.sort();
+    unsigned long sortTimeEnd = clock();
+    unsigned long sortTime = sortTimeEnd - sortTimeStart;
+    
+    printf("Sorting time: %lu ms\n", sortTime);
+    list.print();
+    
 }
